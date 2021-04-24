d67a915b69fc17737a5b1a64e650cb911b5a660f
revathi439 <83110572+revathi439@users.noreply.github.com>
2021-04-24 16:21:45 +0530

    Create Counting Stairs

---
+++ CountingStairs.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+public class Stairs
+{
+public static int Count(int a)
+{
+int n[]=new int[a+1];
+n[0]=1;
+n[1]=1;
+n[2]=2;
+int i;
+for(i=3;i<=a;i++)
+n[i]=n[i-1]+n[i-2]+n[i-3];
+return n[a];
+}
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the number of stairs:");
+int a=sc.nextInt();
+System.out.println("number of ways to reach:"+Count(a));
+}
+}
